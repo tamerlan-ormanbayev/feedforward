a429fff969e740c90441152bc6fcd5ed0e3721ef
yurii.bezborodov <yurii.bezborodov@cs.ucalgary.ca>
2023-11-30 12:38:03 -0700

    Database implementation and index page

--- feedforward/feedforward/urls.py
+++ feedforward/feedforward/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index, contact
+from core.views import index, contact, about
 urlpatterns = [
     path('', index, name="index"),
     path('contact/', contact, name="contact"),
     path('admin/', admin.site.urls),
-]
+    path('about/', about, name='about'),
+]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
